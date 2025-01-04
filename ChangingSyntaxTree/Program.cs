3d76848b10d10c61a43d30047b70c62c9d5d2ef2
Shahab <shahabganji@gmail.com>
2025-01-04 13:47:41 +0100

    feat: add ChangingSyntaxTree project to modify syntax trees
    Added a new project, ChangingSyntaxTree, showcasing syntax tree manipulation with the Roslyn API. Implemented a demonstration to replace "void" return types with "Task" in the provided C# code.

---
+++ ChangingSyntaxTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+// lang=c#
+const string code = """
+                    public class Sample
+                    {
+                        public void Method();
+                        internal void Method2();
+                        
+                        public class Nested
+                        {
+                            public void Method();
+                            public int Method2();
+                        }
+                    }
+                    """;
+
+var syntaxTree = CSharpSyntaxTree.ParseText(code);
+// syntaxTree = SyntaxFactory.ParseSyntaxTree(code);
+
+var root = syntaxTree.GetRoot();
+
+var methods = root
+    .DescendantNodes(_ => true)
+    .OfType<MethodDeclarationSyntax>();
+
+var newRoot = root
+    .ReplaceNodes(methods, (original, _) =>
+    {
+        var returnType = original.ReturnType;
+
+        if (returnType.GetFirstToken().Text != SyntaxFactory.Token(SyntaxKind.VoidKeyword).Text)
+            return original;
+
+        var identifierToken = SyntaxFactory.Identifier("Task")
+            .WithTriviaFrom(returnType.GetFirstToken());
+        // .WithTrailingTrivia(returnType.GetFirstToken().TrailingTrivia)
+        // .WithLeadingTrivia(returnType.GetFirstToken().LeadingTrivia);
+        var identifierNameSyntaxNode = SyntaxFactory.IdentifierName(identifierToken);
+
+        return original.WithReturnType(identifierNameSyntaxNode);
+    });
+
+var newSyntaxTree = syntaxTree.WithRootAndOptions(newRoot, syntaxTree.Options);
+
+Console.WriteLine(newSyntaxTree);
